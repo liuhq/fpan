22194f2fc4784514149ac8ec9febdd0a5cdcdd22
Horace Liu <im.liuhq@gmail.com>
2026-06-15 19:11:25 +0800

    refactor db client to use factory function createDbClient

--- api/db/client.ts
+++ api/db/client.ts
@@ -3,11 +3,21 @@ import { drizzle } from "drizzle-orm/libsql/sqlite3"
 
 import * as schema from "./schema.ts"
 
-export const db = drizzle({
-  connection: {
-    url: Deno.env.get("DB_FILE_NAME")!,
-  },
-  schema,
-})
+/**
+ * create a db client via drizzle, connect to `DB_FILE_NAME`
+ * @returns Drizzle client
+ */
+export const createDbClient = async () => {
+  const db = drizzle({
+    connection: {
+      url: Deno.env.get("DB_FILE_NAME")!,
+    },
+    schema,
+  })
 
-await db.run(sql`PRAGMA foreign_keys = ON`)
+  await db.run(sql`PRAGMA foreign_keys = ON`)
+
+  return db
+}
+
+export type DbClient = Awaited<ReturnType<typeof createDbClient>>
