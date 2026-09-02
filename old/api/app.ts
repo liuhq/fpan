f72ad3084c01f6915bf2933bf569e8399cc9f0f1
Horace Liu <im.liuhq@gmail.com>
2026-09-02 17:47:55 +0800

    refactor: new frontend

--- old/api/app.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono"
-
-import dir from "#dir"
-
-const app = new Hono().route("/dir", dir)
-
-export default app
-export type AppType = typeof app
--- old/web/src/rpc.ts
+++
@@ -1,6 +0,0 @@
-import type { AppType } from "@fpan/api"
-import { hc } from "hono/client"
-
-const client = hc<AppType>("/api")
-
-export default client
